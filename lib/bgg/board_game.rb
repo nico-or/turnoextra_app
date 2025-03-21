780ed9640eba3ebb7dfd5ddad5e4ea5a037e6c0b
nicolas <nicordenes@gmail.com>
2025-03-21 12:38:21 -0300

    feat: add Bgg::BoardGame class

---
+++ lib/bgg/board_game.rb
@@ -0,0 +1,28 @@
+module Bgg
+  class BoardGame
+    def self.from_xml(xml)
+      parsed = Nokogiri::XML(xml)
+      new(
+        id: parsed.root[:objectid],
+        year: parsed.at_xpath("//yearpublished").text,
+        name: parsed.at_xpath("//name[@primary]").text,
+        names: parsed.xpath("//name").map(&:text),
+        description: parsed.at_xpath("//description").text,
+        thumbnail_url: parsed.at_xpath("//thumbnail").text,
+        image_url: parsed.at_xpath("//image").text,
+      )
+    end
+
+    attr_reader :id, :year, :name, :names, :description, :thumbnail_url, :image_url
+
+    def initialize(id:, year:, name:, names:, description:, thumbnail_url:, image_url:)
+      @id = id
+      @year = year
+      @name = name
+      @names = names
+      @description = description
+      @thumbnail_url = thumbnail_url
+      @image_url = image_url
+    end
+  end
+end
