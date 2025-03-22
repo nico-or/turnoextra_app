51eda75d65f64e78ddcfc31be45d57101fc48d8c
nicolas <nicordenes@gmail.com>
2025-03-22 13:11:13 -0300

    feat: add PagesController

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "search#search"
+  root "pages#home"
+
   get "search", to: "search#search"
 end
