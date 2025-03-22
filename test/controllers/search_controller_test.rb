be9813eb18010675757b638ab40d8d91f00ab353
nicolas <nicordenes@gmail.com>
2025-03-22 13:23:31 -0300

    fix: replace invalid route helper methods
    Since pages#home and search#index aren't resources, the _url helper methods weren't working

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
   test "should get search" do
-    get search_search_url
+    get "/search"
     assert_response :success
   end
 end
