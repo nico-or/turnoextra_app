45de33410419222ac575ef2658ef14e2b8a6eb9d
nicolas <nicordenes@gmail.com>
2025-03-23 20:01:55 -0300

    feat: add Listing price utility functions
    update listing#update_date

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,14 @@ class Listing < ApplicationRecord
   belongs_to :boardgame
   has_many :prices
 
+
   def latest_price
-    prices.order(date: :desc).first
+    prices.where(date: update_date)
+          .order(price: :asc)
+          .first
+  end
+
+  def update_date
+    prices.pluck(:date).max
   end
 end
--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ListingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "returns latest update date" do
+    listing = listings(:pandemic_2)
+    date = prices(:price_9).date
+
+    assert_equal date, listing.update_date
+  end
+
+  test "returns latest price" do
+    listing = listings(:pandemic_2)
+    price = prices(:price_9)
+
+    assert_equal price, listing.latest_price
+  end
 end
