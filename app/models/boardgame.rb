f0491f5172846598816db0bdeb0ae10dddd4d001
nicolas <nicordenes@gmail.com>
2025-03-23 20:05:07 -0300

    feat: add Boardgame price utility functions
    update boardgame#update_date

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -1,3 +1,15 @@
 class Boardgame < ApplicationRecord
   has_many :listings
+  has_many :prices, through: :listings
+
+  # lowest price between listings.
+  def best_price
+    prices.where(date: update_date)
+          .order(price: :desc)
+          .first
+  end
+
+  def update_date
+    prices.pluck(:date).max
+  end
 end
--- test/models/boardgame_test.rb
+++ test/models/boardgame_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class BoardgameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "returns date of latest price update" do
+    boardgame = boardgames(:pandemic)
+    date = prices(:price_9).date
+
+    assert_equal date, boardgame.update_date
+  end
+
+  test "returns the best current price" do
+    boardgame = boardgames(:pandemic)
+    price = prices(:price_9)
+
+    assert_equal price, boardgame.best_price
+  end
+
+  test "returns nil if the game has no listings" do
+    boardgame = boardgames(:wingspan)
+
+    assert_nil boardgame.best_price
+  end
 end
