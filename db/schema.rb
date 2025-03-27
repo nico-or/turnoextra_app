5989256c6e89f7a83038ad61ebe0d10564126acf
nicolas <nicordenes@gmail.com>
2025-03-27 19:49:10 -0300

    feat: add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_22_212447) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_27_224452) do
   create_table "boardgames", force: :cascade do |t|
     t.string "title"
     t.integer "bgg_id"
@@ -43,6 +43,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_22_212447) do
     t.string "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "listings", "stores"
