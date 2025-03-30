4555e4cb5cc5c18d6e15b89df9a72d20325b8742
nicolas <nicordenes@gmail.com>
2025-03-30 15:45:13 -0300

    feat: add admin user seed creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(
+  email: ENV['ADMIN_EMAIL'],
+  password_digest:  BCrypt::Password.create(ENV['ADMIN_PASSWORD']),
+  admin: true
+)
