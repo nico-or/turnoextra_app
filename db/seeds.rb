de7d582ba342f99977b47207c0ca57524a295868
nicolas <nicordenes@gmail.com>
2025-03-31 14:52:44 -0300

    seeds: add bgg ranks dump csv import

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,31 @@ User.create(
   password_digest:  BCrypt::Password.create(ENV['ADMIN_PASSWORD']),
   admin: true
 )
+
+
+## Seed BGG boardgame data
+# source: https://boardgamegeek.com/data_dumps/bg_ranks
+# about: https://boardgamegeek.com/wiki/page/BGG_XML_API2#toc1
+
+require 'csv'
+
+filepath = 'db/seeds/boardgames_ranks.csv'
+options = { headers: true }
+
+COUNT_LIMIT = 1_000 if Rails.env.development?
+BATCH_SIZE = 1_000
+
+Boardgame.transaction do
+  csv_enum = CSV.foreach(filepath, **options)
+  csv_enum = csv_enum.first(COUNT_LIMIT) if COUNT_LIMIT
+
+  csv_enum.each_slice(BATCH_SIZE)  do |rows|
+    games = rows.map do |row|
+      { bgg_id: row['id'].to_i, title: row['name'] }
+    end
+
+    # ActiveRecord::Relation#insert_all skips model validations!
+    # Ensure that the CSV data is valid for mass import
+    Boardgame.insert_all(games)
+  end
+end
