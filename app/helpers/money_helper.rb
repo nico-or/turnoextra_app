da1de528ecf8a4b5445eb7795c92a7b9f62306a4
nicolas <nicordenes@gmail.com>
2025-03-31 15:46:57 -0300

    refactor: use activesupport for price formatting

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -1,6 +1,6 @@
 module MoneyHelper
   def format_price(price)
     price = price.price if price.instance_of? Price
-    "$" + price.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1.').reverse
+    number_to_currency(price, precision: 0, delimiter: ".")
   end
 end
