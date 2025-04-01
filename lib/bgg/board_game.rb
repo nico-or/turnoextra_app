7ba057c84105e5627eb5b244acb597bed4aa1f8c
nicolas <nicordenes@gmail.com>
2025-03-31 22:46:17 -0300

    refactor: remove methods for data classes
    With the new Query classes performing all the xml parsing and object instantiation,
    there's no need to keep BoardGame and SearchResult .from_xml class methods.
    
    With that the tests for these classes became useless.
    
    This also puts in evidence that these classes have no behaviour!

--- lib/bgg/board_game.rb
+++ lib/bgg/board_game.rb
@@ -1,22 +1,6 @@
 module Bgg
   class BoardGame
-    def self.from_xml(xml)
-      parsed = Nokogiri::XML(xml)
-
-      # valid request, but item not found
-      return if parsed.at_xpath("//error")
-
-      new(
-        id: parsed.root[:objectid],
-        year: parsed.at_xpath("//yearpublished").text,
-        name: parsed.at_xpath("//name[@primary]").text,
-        names: parsed.xpath("//name").map(&:text),
-        description: parsed.at_xpath("//description").text,
-        thumbnail_url: parsed.at_xpath("//thumbnail").text,
-        image_url: parsed.at_xpath("//image").text,
-      )
-    end
-
+    
     attr_reader :id, :year, :name, :names, :description, :thumbnail_url, :image_url
 
     def initialize(id:, year:, name:, names:, description:, thumbnail_url:, image_url:)
