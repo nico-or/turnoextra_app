cbd08c0e5882f811486e57ea64c52fff148826bc
nicolas <nicordenes@gmail.com>
2025-04-07 13:50:46 -0400

    view: Order boardgame#index by title

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -1,7 +1,7 @@
 class BoardgamesController < ApplicationController
   skip_before_action :authorize_user, only: %i[index show]
   def index
-    @boardgames = Boardgame.where.associated(:listings).distinct
+    @boardgames = Boardgame.where.associated(:listings).distinct.order(:title)
 
     if params[:q].present?
       @boardgames = @boardgames.where("boardgames.title LIKE ?", "%#{params[:q]}%")
