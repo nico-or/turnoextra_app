72ff0492a29cbc61b0162b00aab3344505fc975e
nicolas <nicordenes@gmail.com>
2025-04-11 17:18:11 -0400

    refactor: Add validations to Boardgame model

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -2,6 +2,9 @@ class Boardgame < ApplicationRecord
   has_many :listings
   has_many :prices, through: :listings
 
+  validates :title, presence: true
+  validates :bgg_id, presence: true, uniqueness: true, numericality: { only_integer: true, greater_than: 0 }
+
   def update_date
     prices.pluck(:date).max
   end
--- test/models/boardgame_test.rb
+++ test/models/boardgame_test.rb
@@ -7,4 +7,36 @@ class BoardgameTest < ActiveSupport::TestCase
 
     assert_equal date, boardgame.update_date
   end
+
+  test "should not allow empty titles" do
+    boardgame = Boardgame.new(title: "", bgg_id: "12345")
+    assert_not boardgame.valid?
+  end
+
+  test "should not allow missing titles" do
+    boardgame = Boardgame.new(title: nil, bgg_id: "12345")
+    assert_not boardgame.valid?
+  end
+
+  test "should not allow duplicate bgg_id" do
+    boardgame = Boardgame.new(
+      title: "New Game",
+      bgg_id: boardgames(:pandemic).bgg_id
+    )
+    assert_not boardgame.valid?
+  end
+
+  test "should not allow negative bgg_id" do
+    boardgame = Boardgame.new(title: "New Game", bgg_id: -1)
+    assert_not boardgame.valid?
+  end
+  test "should not allow non-integer bgg_id" do
+    boardgame = Boardgame.new(title: "New Game", bgg_id: "abc")
+    assert_not boardgame.valid?
+  end
+
+  test "should not allow floating point bgg_id" do
+    boardgame = Boardgame.new(title: "New Game", bgg_id: 123.45)
+    assert_not boardgame.valid?
+  end
 end
