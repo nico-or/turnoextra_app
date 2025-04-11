379be53bfb7fb3cfdf58d643152175b929772dde
nicolas <nicordenes@gmail.com>
2025-04-11 18:28:48 -0400

    refactor: Add validations to Listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,9 @@ class Listing < ApplicationRecord
   belongs_to :boardgame, optional: true
   has_many :prices
 
+  validates :title, presence: true
+  validates :url, presence: true
+
   def latest_price_date
     @latest_price_date ||= prices.maximum(:date)
   end
--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,6 +1,39 @@
 require "test_helper"
 
 class ListingTest < ActiveSupport::TestCase
+  def setup
+    @store = stores(:devir)
+    @boardgame = boardgames(:pandemic)
+    @listing_params = {
+      title: "New Game",
+      url: "www.example.com",
+      store_id: @store.id,
+      boardgame_id: @boardgame.id
+    }
+  end
+
+  test "should create a new listing with valid attributes" do
+    listing = Listing.new(@listing_params)
+    assert listing.save
+  end
+
+  test "shold not allow creation without store_id" do
+    params = @listing_params.merge(store_id: nil)
+    listing = Listing.new(params)
+    assert_not listing.valid?
+  end
+  test "shold not allow creation without title" do
+    params = @listing_params.merge(title: nil)
+    listing = Listing.new(params)
+    assert_not listing.valid?
+  end
+
+  test "should not allow creation without url" do
+    params = @listing_params.merge(url: nil)
+    listing = Listing.new(params)
+    assert_not listing.valid?
+  end
+
   test "#latest_price_date returns latest price date" do
     listing = listings(:pandemic_2)
     date = prices(:price_9).date
