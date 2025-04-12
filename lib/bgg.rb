4e1afd9b7cb061e8d3696bbc4fc39fdf7f70fd4e
nicolas <nicordenes@gmail.com>
2025-04-12 16:52:22 -0400

    feat: Add Bgg.uri_for method
    Utility method to generate BoardGameGeek uris for various classes.

--- lib/bgg.rb
+++ lib/bgg.rb
@@ -1,3 +1,22 @@
 module Bgg
   BASE_URI = URI.parse("https://www.boardgamegeek.com")
+
+  class << self
+    def uri_for(boardgame)
+      URI.join(BASE_URI, "/boardgame/", id_for(boardgame).to_s)
+    end
+
+    private
+
+    def id_for(object)
+      case object
+      when Boardgame then object.bgg_id
+      when BoardGame then object.id
+      when SearchResult then object.id
+      when Integer then object
+      when String then object.to_i
+      else raise ArgumentError.new("Expected a Boardgame, Bgg::BoardGame, Integer or String, got #{object.class} instead.")
+      end
+    end
+  end
 end
---
+++ test/lib/bgg_test.rb
@@ -0,0 +1,43 @@
+require "test_helper"
+
+class BggTest < ActiveSupport::TestCase
+  def setup
+    @boardgame =boardgames(:catan)
+    @boardgame_url = URI.parse("https://www.boardgamegeek.com/boardgame/13")
+  end
+  test "Bgg.uri_for a Boardgame" do
+    assert_equal @boardgame_url, Bgg.uri_for(@boardgame)
+  end
+
+  test "Bgg.uri_for a Bgg::BoardGame" do
+    boardgame = Bgg::BoardGame.new(
+      id: 822,
+      name: nil,
+      names: [],
+      year: nil,
+      description: nil,
+      thumbnail_url: nil,
+      image_url: nil
+    )
+    boardgame_url = URI.parse("https://www.boardgamegeek.com/boardgame/822")
+    assert_equal boardgame_url, Bgg.uri_for(boardgame)
+  end
+
+  test "Bgg.uri_for a Bgg::SearchResult" do
+    search_result = Bgg::SearchResult.new(
+      id: 67239,
+      name: nil,
+      year: nil
+    )
+    boardgame_url = URI.parse("https://www.boardgamegeek.com/boardgame/67239")
+    assert_equal boardgame_url, Bgg.uri_for(search_result)
+  end
+
+  test "Bgg.uri_for an Integer" do
+    assert_equal @boardgame_url, Bgg.uri_for(13)
+  end
+
+  test "Bgg.uri_for a String" do
+    assert_equal @boardgame_url, Bgg.uri_for("13")
+  end
+end
