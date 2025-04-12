ed38042dea989a6cffcc922e627caea234b6e1f8
nicolas <nicordenes@gmail.com>
2025-04-12 13:28:35 -0400

    refactor: Add validations to Store model

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,6 @@
 class Store < ApplicationRecord
   has_many :listings
+
+  validates :name, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
 end
--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class StoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @params = {
+      name: "Example Store",
+      url: "http://example.com"
+    }
+  end
+
+  test "should create store" do
+    assert_difference("Store.count") do
+      Store.create(@params)
+    end
+  end
+
+  test "should not create store without name" do
+    @params[:name] = nil
+    store = Store.new(@params)
+    assert_not store.valid?
+    assert_no_difference("Store.count") do
+      store.save
+    end
+  end
+
+  test "should not create store without url" do
+    @params[:url] = nil
+    store = Store.new(@params)
+    assert_not store.valid?
+    assert_no_difference("Store.count") do
+      store.save
+    end
+  end
+
+  test "should not allow duplicate name" do
+    Store.create(@params)
+    assert_no_difference("Store.count") do
+      Store.create(@params.merge(url: "http://example.org"))
+    end
+  end
+
+  test "should not allow duplicate url" do
+    Store.create(@params)
+    assert_no_difference("Store.count") do
+      Store.create(@params.merge(name: "Example Store 2"))
+    end
+  end
 end
