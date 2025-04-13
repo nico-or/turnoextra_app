bf805abc69f0d889947c32acfaf19c77ea014555
nicolas <nicordenes@gmail.com>
2025-04-13 13:34:24 -0400

    refactor: Reduce number of SQL queries required for listing csv
    Reduced number of SQL queries required for listing csv imports by first
    grouping the rows by store_url in memory so a single #create_store call
    is performed instead of one per row.
    
    This is still pretty inneficient and could be improved further by
    using a bulk insert or similar technique. I tried setting listings
    to `accept_nested_attributes_for :prices`, but that didn't work in
    conjuction with the #insert_all or #upsert_all methods.
    
    Benchmark results for 2 files uploads, each uploaded twice:
    the first one creates all the records and the second one skips them.
    
    - 1st: Completed 200 OK in 3020ms (Views: 6.5ms | ActiveRecord: 662.6ms (2281 queries, 0 cached) | GC: 403.1ms)
    - 2nd: Completed 200 OK in 1332ms (Views: 8.4ms | ActiveRecord: 87.0ms (1708 queries, 568 cached) | GC: 102.1ms)
    - 1st: Completed 200 OK in 1655ms (Views: 2.0ms | ActiveRecord: 413.4ms (1569 queries, 0 cached) | GC: 217.9ms)
    - 2nd: Completed 200 OK in 709ms (Views: 2.6ms | ActiveRecord: 46.4ms (1177 queries, 391 cached) | GC: 67.3ms)
    
    - 1st: Completed 200 OK in 2550ms (Views: 57.4ms | ActiveRecord: 727.0ms (1709 queries, 0 cached) | GC: 291.3ms)
    - 2nd: Completed 200 OK in 942ms (Views: 6.7ms | ActiveRecord: 81.9ms (1140 queries, 0 cached) | GC: 130.7ms)
    - 1st: Completed 200 OK in 1535ms (Views: 4.1ms | ActiveRecord: 485.6ms (1178 queries, 0 cached) | GC: 177.1ms)
    - 2nd: Completed 200 OK in 541ms (Views: 7.8ms | ActiveRecord: 75.8ms (786 queries, 0 cached) | GC: 77.9ms)

--- app/services/listing_csv_import_service.rb
+++ app/services/listing_csv_import_service.rb
@@ -8,44 +8,39 @@ class ListingCsvImportService < ApplicationService
   end
 
   def call
-    csv_options = { headers: true, header_converters: :symbol }
-    CSV.foreach(file, **csv_options)
-       .reject { |row| row[:stock] == "false" }
-       .each_slice(BATCH_SIZE) do |rows|
-      ActiveRecord::Base.transaction do
-        rows.each do |row|
-          store = create_store(row)
-          listing = create_listing(store, row)
-          create_price(listing, row)
-        end
+    table = CSV.table(file, converters: nil)
+
+    rows_per_store = table.reject { |row| row[:stock] == "false" }
+                          .group_by { |row| URI.parse(row[:url]).origin }
+
+    rows_per_store.each do |store_url, rows|
+      store = create_store(store_url)
+
+      rows.each do |row|
+        listing = create_listing(store, row)
+        _price = create_price(listing, row)
       end
     end
   end
 
   private
 
-  def create_store(row)
-    store_uri = URI.parse(row[:url])
-    store_name = store_uri.hostname
-    store_url = store_uri.origin
-    Store.find_or_create_by!(url: store_url) do |store|
-      store.name = store_name
+  def create_store(store_url)
+    Store.find_or_create_by(url: store_url) do |store|
+      store.name = URI.parse(store_url).hostname
     end
   end
 
   def create_listing(store, row)
-    listing_title =  row[:title]
-    listing_url =  row[:url]
-    store.listings.find_or_create_by!(url: listing_url) do |listing|
-      listing.title = listing_title
+    Listing.find_or_create_by(url: row[:url]) do |l|
+      l.store = store
+      l.title = row[:title]
     end
   end
 
   def create_price(listing, row)
-    price_amount = row[:price]
-    price_date = row[:date]
-    listing.prices.find_or_create_by!(date: price_date) do |price|
-      price.amount = price_amount
+    listing.prices.find_or_create_by!(date: row[:date]) do |price|
+      price.amount = row[:price]
     end
   end
 end
