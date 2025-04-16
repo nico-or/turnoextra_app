53cf9e386d38aebf14aa0ccdd96ae0e694c769bd
nicolas <nicordenes@gmail.com>
2025-04-16 13:05:46 -0400

    feat: Add Boardgame has_listings and without_images scopes

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -6,6 +6,9 @@ class Boardgame < ApplicationRecord
   validates :title, presence: true
   validates :bgg_id, presence: true, uniqueness: true, numericality: { only_integer: true, greater_than: 0 }
 
+  scope :has_listings, -> { joins(:listings).distinct }
+  scope :without_images, -> { where("image_url IS NULL OR thumbnail_url IS NULL") }
+
   def latest_price_date
     @latest_price_date ||= prices.maximum(:date)
   end
