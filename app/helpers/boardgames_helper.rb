9e303fed63a7c6ce8adc67639a8feea87e72ea58
nicolas <nicordenes@gmail.com>
2025-04-22 16:16:28 -0400

    refactor: Use reference_price for discount_tag

--- app/helpers/boardgames_helper.rb
+++ app/helpers/boardgames_helper.rb
@@ -1,7 +1,12 @@
 module BoardgamesHelper
   def discount_tag(boardgame)
-    if boardgame.respond_to?(:discount_percentage)
-      content_tag(:span, " (-#{boardgame.discount_percentage}%)")
-    end
+    return if boardgame.reference_price == 0
+    return unless boardgame.price < boardgame.reference_price
+
+    abs_difference =  boardgame.price - boardgame.reference_price
+    rel_difference = 1.0 * abs_difference / boardgame.reference_price
+    discount = (100 * rel_difference).to_i
+
+    content_tag(:span, "(#{discount}%)")
   end
 end
