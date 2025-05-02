8bc415330cb0e81c2bb9a4ca91b08b18aa2ee259
nicolas <nicordenes@gmail.comm>
2025-05-02 12:12:12 -0400

    feat: add OpenGraph tags for the boardgames#show pages

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -15,4 +15,14 @@ module MetaTagsHelper
       }
     }
   end
+
+  def opengraph_description_for(boardgame, discount: 0)
+    short_title = truncate(boardgame.title, length: 40)
+
+    if discount < 10
+      I18n.translate("boardgames.show.opengraph.description", title: short_title)
+    else
+      I18n.translate("boardgames.show.opengraph.description_discount", title: short_title, discount: discount)
+    end
+  end
 end
