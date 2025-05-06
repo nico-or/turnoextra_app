3057760940d6df257fcbd5a06a72d005a84df447
nicolas <nicordenes@gmail.com>
2025-05-06 17:33:09 -0400

    feat: Add AdminIdentifications#create method
    Implements manual BGG API identification in two steps.
    
    First the user provides a search query send to the API.
    Them selects between the results the correct boardgame.

--- app/controllers/admin/identifications_controller.rb
+++ app/controllers/admin/identifications_controller.rb
@@ -14,6 +14,22 @@ module Admin
                          .where(boardgame: nil)
                          .where(is_boardgame: true)
                          .where("lower(title) = ?", params[:id])
+
+      @results = params[:query].present? ? Bgg::Versions::XmlV1.search(params[:query]) : []
+    end
+
+    def create
+      boardgame = Boardgame.find_by(bgg_id: params[:bgg_id])
+
+      if boardgame
+        count = Listing.where(id: params[:listing_ids])
+                       .update_all({ boardgame_id: boardgame.id, failed_identification: false })
+
+        flash[:notice] = "Updated #{count} Listings."
+      else
+        flash[:alert] = "Did not find a Boardgame with BGG ID = #{params[:bgg_id]}."
+      end
+      redirect_to admin_identifications_path
     end
   end
 end
