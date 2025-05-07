2b03e4a9c35f42493b9d9d02dbfd2f798df2b012
nicolas <nicordenes@gmail.com>
2025-05-07 18:45:18 -0400

    test(Admin::ContactMessagesController): add missing test cases

--- test/controllers/admin/contact_messages_controller_test.rb
+++ test/controllers/admin/contact_messages_controller_test.rb
@@ -24,13 +24,20 @@ class Admin::ContactMessagesControllerTest < ActionDispatch::IntegrationTest
       assert_response :success
     end
 
-    test "should toggle read status of contact message" do
-      before_status = @contact_message.status
+    test "should toggle read status from unread to read" do
+      @contact_message.unread!
       post toggle_read_admin_contact_message_path(@contact_message)
-      after_status = @contact_message.reload.status
-
-      assert before_status != after_status
+      @contact_message.reload
       assert_redirected_to admin_contact_message_path(@contact_message)
+      assert @contact_message.read?
+    end
+
+    test "should toggle read status from read to unread" do
+      @contact_message.read!
+      post toggle_read_admin_contact_message_path(@contact_message)
+      @contact_message.reload
+      assert_redirected_to admin_contact_message_path(@contact_message)
+      assert @contact_message.unread?
     end
   end
 end
