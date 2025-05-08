0127fbde92241136347a47ae7fff4e302ad42845
nicolas <nicordenes@gmail.com>
2025-05-08 13:37:24 -0400

    test(Bgg::Versions::XmlV1::BoardgameResponseParser): refactor tests

--- test/lib/bgg/versions/xml_v1/boardgame_response_parser_test.rb
+++ test/lib/bgg/versions/xml_v1/boardgame_response_parser_test.rb
@@ -3,50 +3,68 @@ require "test_helper"
 
 module Bgg::Versions::XmlV1
   class BoardgameResponseParserTest < ActiveSupport::TestCase
-    def setup
+    test "#parse! a XML response with a single boardgame" do
       xml = file_fixture("bgg/api/v1/query_single.xml").read
       response = Nokogiri::XML(xml)
-      @boardgames = BoardgameResponseParser.parse!(response)
-      @boardgame = @boardgames.first
+      boardgames = BoardgameResponseParser.parse!(response)
+      assert_equal 1, boardgames.size
+      assert boardgames.is_a? Array
+      assert boardgames.all? { |game| game.is_a? Bgg::Boardgame }
+
+      game = boardgames.first
+      assert_equal "Carcassonne", game.name
+      assert_equal 2000, game.year
+      assert_equal 822, game.id
+      assert game.names.is_a? Array
+      assert_equal 23, game.names.count
+      assert_match %r{__thumb/img/}, game.thumbnail_url
+      assert_match %r{__original/img/}, game.image_url
+      assert_match %r{Carcassonne is a tile placement game}, game.description
     end
 
-    test "#games returns an array of BoardGames" do
-      assert @boardgames.is_a? Array
-      assert @boardgames.all? { |game| game.is_a? Bgg::Boardgame }
-    end
-
-    test "buils the correct BoardGame" do
-      assert_equal 822, @boardgame.id
-      assert_equal 2000, @boardgame.year
-      assert_equal "Carcassonne", @boardgame.name
-      assert_equal 23, @boardgame.names.count
-    end
-  end
-
-  class MultipleBoardgameResponseParserTest < ActiveSupport::TestCase
-    def setup
+    test "#parse! a XML response with multiple boardgames" do
       xml = file_fixture("bgg/api/v1/query_multiple.xml").read
       response = Nokogiri::XML(xml)
-      @boardgames = BoardgameResponseParser.parse!(response)
+      boardgames = BoardgameResponseParser.parse!(response)
+      assert boardgames.is_a? Array
+      assert_equal 2, boardgames.size
+      assert boardgames.all? { |game| game.is_a? Bgg::Boardgame }
+
+      game1 = boardgames[0]
+      assert_equal "Wingspan", game1.name
+      assert_equal 2019, game1.year
+      assert_equal 266192, game1.id
+      assert_equal 18, game1.names.count
+
+      game2 = boardgames[1]
+      assert_equal "Pandemic", game2.name
+      assert_equal 2008, game2.year
+      assert_equal 30549, game2.id
+      assert_equal 24, game2.names.count
     end
 
-    test "#games returns an array of BoardGames" do
-      assert_equal 2, @boardgames.count
-      assert @boardgames.is_a? Array
-      assert @boardgames.all? { |game| game.is_a? Bgg::Boardgame }
-    end
-  end
-
-  class NotFoundBoardgameResponseParserTest < ActiveSupport::TestCase
-    def setup
+    test "#parse! a XML response with no boardgames" do
       xml = file_fixture("bgg/api/v1/query_not_found.xml").read
       response = Nokogiri::XML(xml)
-      @boardgames = BoardgameResponseParser.parse!(response)
+      boardgames = BoardgameResponseParser.parse!(response)
+      assert boardgames.is_a? Array
+      assert boardgames.empty?
     end
 
-    test "#games returns an empty array" do
-      assert @boardgames.empty?
-      assert @boardgames.is_a? Array
+    test "#parse! a XML response with missing fields" do
+      xml = file_fixture("bgg/api/v1/query_single_missing_fields.xml").read
+      response = Nokogiri::XML(xml)
+      boardgames = BoardgameResponseParser.parse!(response)
+      assert boardgames.is_a? Array
+      assert_equal 1, boardgames.size
+
+      game = boardgames.first
+
+      assert_nil game.name
+      assert_nil game.year
+      assert_nil game.description
+      assert_nil game.thumbnail_url
+      assert_nil game.image_url
     end
   end
 end
