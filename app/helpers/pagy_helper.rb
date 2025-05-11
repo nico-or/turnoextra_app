ec2a61f22a46085ce9558cf6199462c395ec037b
nicolas <nicordenes@gmail.comm>
2025-05-11 16:28:39 -0400

    style: remove picocss

--- app/helpers/pagy_helper.rb
+++ app/helpers/pagy_helper.rb
@@ -1,6 +1,6 @@
 module PagyHelper
   def pagy_nav_link(label:, page:, aria_label:, path_helper:, rel:)
-    options = { role: "button", "aria-label": aria_label, rel: rel }
+    options = { "aria-label": aria_label, rel: rel }
 
     if page
       link_to label, path_helper.call(page), **options.merge("aria-disabled": false)
