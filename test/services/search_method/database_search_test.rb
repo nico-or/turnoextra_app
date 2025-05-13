20a2d8da57c7d003d0079aa3d663cc987a112aaa
nicolas <nicordenes@gmail.comm>
2025-05-13 18:10:31 -0400

    fix(SearchMethod::DatabaseSearch): apply more strict conditions
    There was a bug, where boardgames with long titles and/or expansion could not be
    searched in the Admin section.
    
    Example: searching for "unstable unicorns anything" would always return the
    "unstable unicorns" boardgame and never search the Bgg::Api
    
    Also make it ignore unranked games

--- test/services/search_method/database_search_test.rb
+++ test/services/search_method/database_search_test.rb
@@ -2,8 +2,13 @@ require "test_helper"
 
 module SearchMethod
   class DatabaseSearchTest < ActiveSupport::TestCase
-    test "#call with a boardgame that is in the database" do
-      boardgame = Boardgame.first
+    def setup
+      Rails.cache.clear
+    end
+
+    test "#call with a ranked boardgame that is in the database" do
+      boardgame = Boardgame.create!(title: 'test game', rank: 1, bgg_id: 1, year: 2010)
+
       @service = DatabaseSearch.new(boardgame.title)
       results = @service.call
 
@@ -16,11 +21,24 @@ module SearchMethod
       assert_equal boardgame.bgg_id, result.bgg_id
     end
 
-    test "#call with a listing that is in the database" do
+    test "#call with an unranked boardgame that is in the database" do
+      boardgame = Boardgame.create!(title: 'test game', rank: 0, bgg_id: 1, year: 2010)
+
+      @service = DatabaseSearch.new(boardgame.title)
+      results = @service.call
+
+      assert results.is_a? Array
+      assert results.empty?
+    end
+
+    test "#call with a ranked listing that is in the database" do
       listing = Listing.joins(:boardgame).first
       assert_not_nil listing
       boardgame = listing.boardgame
       assert_not_nil boardgame
+
+      boardgame.rank = 1
+      boardgame.save!
 
       @service = DatabaseSearch.new(listing.title)
       results = @service.call
@@ -32,6 +50,22 @@ module SearchMethod
       assert_equal listing.title, result.title
       assert_equal boardgame.year, result.year
       assert_equal boardgame.bgg_id, result.bgg_id
+    end
+
+    test "#call with an unranked listing that is in the database" do
+      listing = Listing.joins(:boardgame).first
+      assert_not_nil listing
+      boardgame = listing.boardgame
+      assert_not_nil boardgame
+
+      boardgame.rank = 0
+      boardgame.save!
+
+      @service = DatabaseSearch.new(listing.title)
+      results = @service.call
+
+      assert results.is_a? Array
+      assert results.empty?
     end
 
     test "#call with a boardgame that is not in the database" do
