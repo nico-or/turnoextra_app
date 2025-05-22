ea6219c8fc82f120f1edb18d00b17211e5b67473
nicolas <nicordenes@gmail.com>
2025-05-22 12:27:28 -0400

    feat: add Boardgame#with_similar_title scope for trigram similarity search

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -13,6 +13,17 @@ class Boardgame < ApplicationRecord
   scope :has_listings, -> { joins(:listings).distinct }
   scope :without_images, -> { where("image_url IS NULL OR thumbnail_url IS NULL") }
   scope :with_title_like, ->(title) { where("LOWER(boardgames.title) LIKE LOWER(?)", "%#{title}%") }
+  scope :with_similar_title, ->(title, threshold = 0.8) do
+    joins(:boardgame_names)
+    .select(
+      "boardgames.*",
+      "boardgame_names.*",
+      sanitize_sql_array([ "similarity(boardgame_names.value, ?) AS similarity", title ])
+    )
+    .where("similarity(boardgame_names.value, ?) > ?", title, threshold)
+    .order("similarity desc")
+    .distinct
+  end
 
   def latest_price_date
     @latest_price_date ||= prices.maximum(:date)
