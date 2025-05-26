9d21a0fe4045b779a7a18bf76560841abef035ae
nicolas <nicordenes@gmail.comm>
2025-05-26 18:41:58 -0400

    feat(Text::Trigram): add lib module for similarity calculations

---
+++ lib/text/trigram.rb
@@ -0,0 +1,27 @@
+module Text
+  module Trigram
+    class << self
+      def similarity(str1, str2)
+        trgm_1 = generate_trigrams(str1)
+        trgm_2 = generate_trigrams(str2)
+
+        all_trgm = (trgm_1 | trgm_2)
+        common_trgm = (trgm_1 & trgm_2)
+
+        return 0.0 if all_trgm.empty?
+
+        common_trgm.size.to_f / all_trgm.size
+      end
+
+      private
+
+      # Helper method to generate trigrams from a string
+      #   This method adds two spaces at the beginning and one space at the end of the string
+      #   to give more weight to the start of the string
+      def generate_trigrams(string)
+        normalized_string = StringNormalizationService.normalize_string(string)
+        "  #{normalized_string} ".chars.each_cons(3).map(&:join)
+      end
+    end
+  end
+end
