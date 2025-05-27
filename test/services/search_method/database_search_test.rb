b0d8cf124288b1d0e6b786131eed1f638360a0bd
nicolas <nicordenes@gmail.com>
2025-05-27 17:06:41 -0400

    refactor(DatabaseSearch): use postgresql similarity methods for searching

--- test/services/search_method/database_search_test.rb
+++ test/services/search_method/database_search_test.rb
@@ -31,14 +31,25 @@ module SearchMethod
       assert results.empty?
     end
 
-    test "#call with a ranked listing that is in the database" do
-      listing = Listing.joins(:boardgame).first
-      assert_not_nil listing
-      boardgame = listing.boardgame
-      assert_not_nil boardgame
+    test "#call with a ranked boardgame with a boardgame_name that is in the database" do
+      boardgame = Boardgame.create!(title: "test game", rank: 1, bgg_id: 1, year: 2010)
+      alternative_name = boardgame.boardgame_names.create!(value: "alternative name")
 
-      boardgame.rank = 1
-      boardgame.save!
+      @service = DatabaseSearch.new(alternative_name.value)
+      results = @service.call
+
+      assert results.is_a? Array
+      assert results.all? { |result| result.is_a? Bgg::SearchResult }
+
+      result = results.first
+      assert_equal alternative_name.value, result.title
+      assert_equal boardgame.year, result.year
+      assert_equal boardgame.bgg_id, result.bgg_id
+    end
+
+    test "#call with a ranked listing that is in the database" do
+      boardgame = Boardgame.create!(title: "test game", rank: 1, bgg_id: 1, year: 2010)
+      listing = boardgame.listings.create!(title: "test listing", store: Store.first, url: "https://example.com")
 
       @service = DatabaseSearch.new(listing.title)
       results = @service.call
@@ -53,10 +64,8 @@ module SearchMethod
     end
 
     test "#call with an unranked listing that is in the database" do
-      listing = Listing.joins(:boardgame).first
-      assert_not_nil listing
-      boardgame = listing.boardgame
-      assert_not_nil boardgame
+      boardgame = Boardgame.create!(title: "test game", rank: 0, bgg_id: 1, year: 2010)
+      listing = boardgame.listings.create!(title: "test listing", store: Store.first, url: "https://example.com")
 
       boardgame.rank = 0
       boardgame.save!
