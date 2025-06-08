cc8d45e0227cf035c9744d2e887d5ea8794cd7c0
nicolas <nicordenes@gmail.com>
2025-06-07 21:40:13 -0400

    wip: fix modern browser issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern if Rails.env.production?
+  # https://github.com/rails/rails/blob/main/actionpack/lib/action_controller/metal/allow_browser.rb
+  # Allow safari >= 16 due to user request.
+  # We aren't using any 'modern' browser features, so it should be ok (?)
+  allow_browser versions: { safari: 16, chrome: 120, firefox: 121, opera: 106, ie: false } if Rails.env.production?
 
   before_action :set_current_user
 
