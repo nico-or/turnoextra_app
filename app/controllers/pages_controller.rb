792c53b6039e44e627f4f45c84d31fd701fc1366
nicolas <nicordenes@gmail.com>
2025-06-11 13:33:58 -0400

    refactor(pages#home): extract private methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,42 +1,67 @@
 class PagesController < ApplicationController
   def home
-    reference_date = Price.latest_update_date
-    yesterday_date = reference_date - 1.day
+    @deals = top_discounted_deals
+    @new_deals = new_price_drops
+    @top_bgg = top_ranked_boardgames
+    @most_viewed = most_viewed_boardgames
+  end
 
-    yesterday_prices = Boardgame.joins(:prices)
-    .where(prices: { date: yesterday_date })
-    .select(
-      "boardgames.id AS boardgame_id",
-      "MIN(prices.amount) AS amount"
-    )
-    .group("boardgames.id")
+  private
 
-    base_query = Boardgame.joins(:daily_boardgame_deals)
+  def reference_date
+    @reference_date ||= Price.latest_update_date
+  end
+
+  def yesterday_date
+    reference_date - 1.day
+  end
+
+  def yesterday_prices
+    @yesterday_prices ||= Boardgame.joins(:prices)
+      .where(prices: { date: yesterday_date })
+      .select(
+        "boardgames.id AS boardgame_id",
+        "MIN(prices.amount) AS amount"
+      )
+      .group("boardgames.id")
+  end
+
+  def base_query
+    Boardgame.joins(:daily_boardgame_deals)
       .select(
         "boardgames.id",
         "boardgames.title",
         "boardgames.thumbnail_url",
         "daily_boardgame_deals.discount",
         "daily_boardgame_deals.best_price",
-        "daily_boardgame_deals.reference_price")
+        "daily_boardgame_deals.reference_price"
+      )
       .limit(8)
+  end
 
-    @deals = base_query
+  def top_discounted_deals
+    base_query
       .where("daily_boardgame_deals.discount > 0")
       .order("discount DESC")
+  end
 
-    @new_deals = base_query
+  def new_price_drops
+    base_query
       .with(yesterday_prices: yesterday_prices)
       .joins("JOIN yesterday_prices ON yesterday_prices.boardgame_id = boardgames.id")
       .where("yesterday_prices.amount > daily_boardgame_deals.best_price")
       .order("discount DESC")
+  end
 
-    @top_bgg = base_query
+  def top_ranked_boardgames
+    base_query
       .where("boardgames.rank > 0")
       .order("boardgames.rank ASC")
+  end
 
+  def most_viewed_boardgames
     date_window = (reference_date - 7.days..reference_date)
-    @most_viewed = base_query
+    base_query
       .joins(:impressions)
       .where(impressions: { date: date_window })
       .group(
