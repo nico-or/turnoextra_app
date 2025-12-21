c81af9184b98f4ac3b653adabe4d3d8159179c49
nicolas <nicordenes@gmail.com>
2025-12-21 20:52:42 -0300

    feat(SearchController): use the new search_value column to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,27 +1,37 @@
 class SearchController < ApplicationController
   def search
-    query = params[:q]
+    query = Text::Normalization.search_value(params[:q])
     quoted_query = ActiveRecord::Base.connection.quote(query)
 
-    boardgames = Boardgame
-      .joins(:daily_boardgame_deals)
-      .joins(:boardgame_names)
-      .where("boardgame_names.value %> ?", query)
+    similarities = BoardgameName
       .select(
-        "boardgames.id",
-        "boardgames.title",
-        "boardgames.thumbnail_url",
-        "daily_boardgame_deals.discount",
-        "daily_boardgame_deals.best_price",
-        "daily_boardgame_deals.reference_price",
-        "MAX(word_similarity(#{quoted_query}, boardgame_names.value)) AS similarity")
+        "boardgame_id",
+        "word_similarity(#{quoted_query}, search_value) AS sim")
+      .where("? <% search_value", query)
+
+    boardgames = BoardgameDeal
+      .with(similarities:)
+      .joins("JOIN similarities s ON s.boardgame_id = boardgame_deals.id")
+      .select(
+          "id",
+          "title",
+          "thumbnail_url",
+          "rel_discount_100 AS discount",
+          "t_price AS best_price",
+          "m_price AS reference_price")
       .group(
-        "boardgames.id",
-        "boardgames.thumbnail_url",
-        "daily_boardgame_deals.discount",
-        "daily_boardgame_deals.best_price",
-        "daily_boardgame_deals.reference_price")
-      .order(Arel.sql("MAX(word_similarity(#{quoted_query}, boardgame_names.value)) DESC"))
+        "id",
+        "rank",
+        "title",
+        "thumbnail_url",
+        "rel_discount_100",
+        "t_price",
+        "m_price"
+      )
+      .order(
+        "MAX(sim) DESC",
+        Arel.sql("NULLIF(rank, 0) ASC NULLS LAST"),
+        "title ASC")
 
     @pagy, @boardgames = pagy(boardgames, limit: 12)
   end
