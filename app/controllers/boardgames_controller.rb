5f04277b9e799b447cd5cfeb0e8e794d6d103563
nicolas <nicordenes@gmail.com>
2025-12-22 14:37:05 -0300

    refactor(BoardgamesController): remove  #old_index and references to daily_boardgame_deals

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -1,29 +1,15 @@
 class BoardgamesController < ApplicationController
   def index
-    boardgames = case ENV["OLD_HOME"]
-    when "1"
-      Boardgame
-        .joins(:daily_boardgame_deals)
-        .select(
-          "boardgames.id",
-          "boardgames.title",
-          "boardgames.thumbnail_url",
-          "daily_boardgame_deals.discount",
-          "daily_boardgame_deals.best_price",
-          "daily_boardgame_deals.reference_price")
-        .order(:title)
-        .distinct
-    else
-      BoardgameDeal
-        .select(
-          :id, :thumbnail_url, :title,
-          "t_price AS best_price",
-          "m_price AS reference_price",
-          "rel_discount_100 AS discount",
-        )
-        .where("t_price > 0")
-        .order(:title)
-    end
+    boardgames = BoardgameDeal
+      .select(
+        "id",
+        "title",
+        "thumbnail_url",
+        "t_price AS best_price",
+        "m_price AS reference_price",
+        "rel_discount_100 AS discount")
+      .where("t_price > 0")
+      .order(:title)
 
     @pagy, @boardgames = pagy(boardgames, limit: 12)
   end
