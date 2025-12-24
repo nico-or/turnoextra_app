f9cdd01477efd225a7f563139fbc200c571ecf85
nicolas <nicordenes@gmail.com>
2025-12-24 16:57:18 -0300

    feat(Current): add bot attribute to filter visitors activity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
   # We aren't using any 'modern' browser features, so it should be ok (?)
   allow_browser versions: { safari: 16, chrome: 120, firefox: 121, opera: 106, ie: false } if Rails.env.production?
 
-  before_action :set_current_user
   before_action { Pagy::I18n.locale = "es" }
+  before_action :set_current_user
+  before_action :set_bot
 
   include Pagy::Method
 
@@ -14,5 +15,10 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def set_bot
+    browser = Browser.new(request.user_agent)
+    Current.bot = browser.bot?
   end
 end
