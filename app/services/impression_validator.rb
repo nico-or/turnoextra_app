4cefee05d7967f7c3f6a92ee72e6e50e31832799
nicolas <nicordenes@gmail.com>
2025-12-24 19:46:50 -0300

    feat: add bot detection for Impression tracking

--- app/services/impression_validator.rb
+++ app/services/impression_validator.rb
@@ -1,19 +1,25 @@
 class ImpressionValidator
-  def initialize(user:)
+  def initialize(user:, user_agent:)
     @user = user
+    @user_agent = user_agent
   end
 
   def worthy?
     return false if is_admin?
+    return false if is_bot?
 
     true
   end
 
   private
 
-  attr_reader :user
+  attr_reader :user, :user_agent
 
   def is_admin?
     user&.admin?
+  end
+
+  def is_bot?
+    Browser.new(user_agent).bot?
   end
 end
