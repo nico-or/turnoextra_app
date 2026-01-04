77aecc8a6d20e8b469ba2aacf829f25b94f554fc
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-04 13:57:24 -0300

    test(ImpressionCreatorTest): use visitor helper method

--- test/services/impressions/impression_creator_test.rb
+++ test/services/impressions/impression_creator_test.rb
@@ -1,18 +1,13 @@
 require "test_helper"
 
 class Impressions::ImpressionCreatorTest < ActiveSupport::TestCase
-  REAL_UA = "REAL USER AGENT"
-  BOT_UA = "BOT USER AGENT"
-
   setup do
     @boardgame = Boardgame.first
     @impression = @boardgame.impressions.create!(date: Date.current, count: 0)
   end
 
   test "visitors do create impressions" do
-    user = nil
-    user_agent = REAL_UA
-    visitor = Visitor.new(user:, user_agent:)
+    visitor = anonymous_visitor
     impression_creator = Impressions::ImpressionCreator.new(@boardgame, visitor:)
 
     assert_difference -> { @impression.reload.count }, 1 do
@@ -21,9 +16,7 @@ class Impressions::ImpressionCreatorTest < ActiveSupport::TestCase
   end
 
   test "regular user do create impressions" do
-    user = users(:user)
-    user_agent = REAL_UA
-    visitor = Visitor.new(user:, user_agent:)
+    visitor = user_visitor
     impression_creator = Impressions::ImpressionCreator.new(@boardgame, visitor:)
 
     assert_difference -> { @impression.reload.count }, 1 do
@@ -32,9 +25,7 @@ class Impressions::ImpressionCreatorTest < ActiveSupport::TestCase
   end
 
   test "admin do not create impressions" do
-    user = users(:admin)
-    user_agent = REAL_UA
-    visitor = Visitor.new(user:, user_agent:)
+    visitor = admin_visitor
     impression_creator = Impressions::ImpressionCreator.new(@boardgame, visitor:)
 
     assert_no_difference -> { @impression.reload.count } do
@@ -43,9 +34,7 @@ class Impressions::ImpressionCreatorTest < ActiveSupport::TestCase
   end
 
   test "bot do not create impressions" do
-    user = nil
-    user_agent = BOT_UA
-    visitor = Visitor.new(user:, user_agent:)
+    visitor = bot_visitor
     impression_creator = Impressions::ImpressionCreator.new(@boardgame, visitor:)
 
     assert_no_difference -> { @impression.reload.count } do
@@ -54,17 +43,12 @@ class Impressions::ImpressionCreatorTest < ActiveSupport::TestCase
   end
 
   test "known ip ranges do not create impressions" do
-    user = nil
-    user_agent = REAL_UA
-    remote_ips = [ "74.125.151.33" ]
+    ip_address = "74.125.151.33"
+    visitor = anonymous_visitor(ip_address:)
+    impression_creator = Impressions::ImpressionCreator.new(@boardgame, visitor:)
 
-    remote_ips.each do |ip_address|
-      visitor = Visitor.new(user:, user_agent:, ip_address:)
-      impression_creator = Impressions::ImpressionCreator.new(@boardgame, visitor:)
-
-      assert_no_difference -> { @impression.reload.count } do
-        impression_creator.create
-      end
+    assert_no_difference -> { @impression.reload.count } do
+      impression_creator.create
     end
   end
 end
