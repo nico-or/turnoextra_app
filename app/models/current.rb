7a2d8ad95167e3f463479aa543a3c5dc57f24a1d
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-04 11:13:38 -0300

    feat(Visitor): add ip_address attribute

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,11 +1,13 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :user
   attribute :user_agent
+  attribute :ip_address
 
   def visitor
     Visitor.new(
       user: user,
-      user_agent: user_agent
+      user_agent: user_agent,
+      ip_address: ip_address
     )
   end
 end
