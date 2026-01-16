67c34d2050ec0ce7ffcee27c65a88fdfea6dc560
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-16 18:25:56 -0300

    test(ContactMessageTest): extract contact_message creation

--- test/models/contact_message_test.rb
+++ test/models/contact_message_test.rb
@@ -8,78 +8,59 @@ class ContactMessageTest < ActiveSupport::TestCase
       body: "This is a test message.",
       user_agent: "Mozilla/5.0"
     }
+    @contact_message = ContactMessage.new(@default_params)
   end
 
-  test "should be valid with default params" do
-    contact_message = ContactMessage.new(@default_params)
-    assert contact_message.valid?
+  # Default Status
+
+  test "should default to subject: general" do
+    assert @contact_message.general?
   end
 
-  test "should be unread by default" do
-    contact_message = ContactMessage.new(@default_params)
-    assert contact_message.unread?
+  test "should default status: unread" do
+    assert @contact_message.unread?
   end
+
+  # Validations
 
   test "should set status to read when marked as read" do
-    contact_message = ContactMessage.create(@default_params)
-    contact_message.read!
-    assert contact_message.read?
+    @contact_message.read!
+    assert @contact_message.read?
   end
 
   test "should be valid without a name" do
-    @default_params.delete(:name)
-    contact_message = ContactMessage.new(@default_params)
-    assert contact_message.valid?
+    @contact_message.name = nil
+    assert @contact_message.valid?
   end
 
   test "should be valid without an email" do
-    @default_params.delete(:email)
-    contact_message = ContactMessage.new(@default_params)
-    assert contact_message.valid?
-
-    contact_message.save
-    assert_nil contact_message.email
-  end
-
-  test "should default to 'general' subject if none is provided" do
-    @default_params.delete(:subject)
-    contact_message = ContactMessage.new(@default_params)
-    assert contact_message.general?
-  end
-
-  test "shoudl default to unread status if none is provided" do
-    @default_params.delete(:status)
-    contact_message = ContactMessage.new(@default_params)
-    assert contact_message.unread?
+    @contact_message.email = nil
+    assert @contact_message.valid?
   end
 
   test "should not be valid with an invalid email format" do
-    @default_params[:email] = "invalid_email"
-    contact_message = ContactMessage.new(@default_params)
-    assert_not contact_message.valid?
+    @contact_message.email = "invalid_email"
+    assert_not @contact_message.valid?
   end
 
   test "should not be valid without a body" do
-    @default_params.delete(:body)
-    contact_message = ContactMessage.new(@default_params)
-    assert_not contact_message.valid?
+    @contact_message.body = nil
+    assert_not @contact_message.valid?
   end
 
   test "should downcase the email before saving" do
-    @default_params[:email] = "TEST@EXAMPLE.COM"
-    contact_message = ContactMessage.new(@default_params)
-    contact_message.save!
-    assert_equal "test@example.com", contact_message.email
+    @contact_message.email = "TEST@EXAMPLE.COM"
+    assert @contact_message.valid? # force validation hooks
+    assert_equal "test@example.com", @contact_message.email
   end
+
+  # Relations
 
   test "should allow polymorphic relationship with contactable" do
     contactable = Boardgame.first
-    params = @default_params.merge(contactable:)
-
-    contact_message = ContactMessage.new(params)
-
-    assert contact_message.valid?
-    assert contact_message.contactable_id = contactable.id
-    assert contact_message.contactable_type = contactable.class.name
+    @contact_message.contactable = contactable
+    assert @contact_message.valid?
+    assert @contact_message.contactable_id = contactable.id
+    assert @contact_message.contactable_type = contactable.class.name
   end
 end
