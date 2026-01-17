6c3c0064186da8422a1d989101ea0bba84d0662a
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-16 21:27:43 -0300

    refactor(ContactMessagesController): extract #set_contact_message method

--- app/controllers/admin/contact_messages_controller.rb
+++ app/controllers/admin/contact_messages_controller.rb
@@ -1,28 +1,31 @@
 class Admin::ContactMessagesController < AdminController
+  before_action :set_contact_message, except: [ :index ]
   def index
     @contact_messages = ContactMessage.all.order(created_at: :desc)
   end
 
   def show
-    @contact_message = ContactMessage.find(params[:id])
     @contact_message.update(read: true) unless @contact_message.read?
   end
 
   def mark_addressed
-     @contact_message = ContactMessage.find(params[:id])
      @contact_message.update(status: :addressed, archived: true)
      redirect_to admin_contact_message_path(@contact_message)
   end
 
   def mark_spam
-    @contact_message = ContactMessage.find(params[:id])
     @contact_message.update(status: :dismissed, archived: true, spam: true)
     redirect_to admin_contact_message_path(@contact_message)
   end
 
   def reset_status
-    @contact_message = ContactMessage.find(params[:id])
     @contact_message.update(status: :pending, spam: false, archived: false, read: false)
     redirect_to admin_contact_message_path(@contact_message)
+  end
+
+  private
+
+  def set_contact_message
+    @contact_message = ContactMessage.find(params[:id])
   end
 end
