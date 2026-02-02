645094fde4d8d7d0a0290592a5734a581bd40395
Nicolás Órdenes <nicordenes@gmail.com>
2026-02-02 17:28:37 -0300

    style: reduce line length on StoreSuggestion controllers

--- app/controllers/admin/store_suggestions_controller.rb
+++ app/controllers/admin/store_suggestions_controller.rb
@@ -1,10 +1,11 @@
 module Admin
   class StoreSuggestionsController < AdminController
     def index
-      @store_suggestions_tally = StoreSuggestion.select("store_suggestions.url", "COUNT(*) AS count")
-                                                .group(:url)
-                                                .order("count DESC")
-                                                .limit(10)
+      @store_suggestions_tally = StoreSuggestion
+        .select("store_suggestions.url", "COUNT(*) AS count")
+        .group(:url)
+        .order("count DESC")
+        .limit(10)
     end
   end
 end
--- app/controllers/store_suggestions_controller.rb
+++ app/controllers/store_suggestions_controller.rb
@@ -2,10 +2,11 @@ class StoreSuggestionsController < ApplicationController
   rate_limit to: 5, within: 1.minute, with: -> { redirect_to new_store_suggestion_path, alert: "Rate limit exceeded. Please try again later." }, only: [ :create ]
 
   def index
-    @store_suggestions_tally = StoreSuggestion.select("store_suggestions.url", "COUNT(*) AS count")
-                                              .group(:url)
-                                              .order("count DESC")
-                                              .limit(10)
+    @store_suggestions_tally = StoreSuggestion
+    .select("store_suggestions.url", "COUNT(*) AS count")
+    .group(:url)
+    .order("count DESC")
+    .limit(10)
   end
 
   def new
